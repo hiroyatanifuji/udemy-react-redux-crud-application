a8750ab983976f0e9e9aed264b3ec6c3821761fd
谷藤弘弥 <tanifujihiroya@tanifujihiroyanoMacBook-puro.local>
2018-08-22 15:46:39 +0900

    create prop-types

--- udemy-react-redux-crud-application/src/App.js
+++ udemy-react-redux-crud-application/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
  const profiles = [
   { name: "taro", age: 10},
-  { name: "hanako", age: 8}
+  { name: "hanako", age: 8},
+  { name: "noName", age: 6}
 ]
   
  return (
@@ -20,4 +22,10 @@ const App = () => {
 const User = (props) => {
  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
 }
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
+}
+
 export default App;
